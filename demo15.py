7a313da280098e0c8d1db707dcfa1525f08c8489
老阿張 <laoazhang@163.com>
2023-11-08 14:40:03 +0800

    函数study

---
+++ demo15.py
@@ -0,0 +1,12 @@
+str1 = "itheima"
+str2 = "itcast"
+str3 = "python"
+def my_len(data):
+    count = 0
+    for i in data:
+        count += 1
+    print(f"字符串{data}的长度是：{count}")
+
+my_len(str1)
+my_len(str2)
+my_len(str3)
